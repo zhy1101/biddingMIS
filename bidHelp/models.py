f02ffc290704bdf23845fd701d1f8c733df1d44f
zhy1101 <18073543511@163.com>
2019-04-10 20:56:22 +0800

    2019/4/10

--- bidHelp/models.py
+++ bidHelp/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class User(models.Model):
+    uID = models.CharField(max_length=7)
     uName = models.CharField(max_length=10)
+    uAge = models.IntegerField()
+    uGender = models.CharField(max_length=2)
+    uKind = models.CharField(max_length=2)
+    uPassword  = models.CharField(max_length=10)
+
+class Client(models.Model):
+    cID = models.CharField()
