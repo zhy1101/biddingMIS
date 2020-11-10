719ba3cc71440fa6d82be0030aaa6c9ff8b2399c
zhy1101 <18073543511@163.com>
2020-11-10 09:46:41 +0800

    2019/5/29

--- biddingMIS/settings.py
+++ biddingMIS/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bidHelp',
         'USER': 'root',
-        'PASSWORD': 'zhy2153411',
+        'PASSWORD': '*********',
         'PORT': '3306'
     }
 }
